0d7d7007a8b7e9ade89eb0ee540b3dc3c7ab7e2b
Bárbara <babyr.oliveira@gmail.com>
2020-03-20 15:18:25 -0300

    Resolução do exercício proposto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o seu primeiro nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Escreva o seu sobrenome:");
+            string sobrenome = Console.ReadLine();
+            Console.Write("Seu nome completo: " );
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.Write("Nome de catálogo: ");
+            Console.WriteLine(sobrenome + ", " + nome);
+
+        }
+    }
+}
